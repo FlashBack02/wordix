76b09e982c2036982d85051d8cc5c8362c5d3f26
flashback02 <arireile02@gmail.com>
2023-11-14 12:05:47 -0300

    agregue la funcion de sumar puntos y la de mostrar partidas

--- prueba.php
+++ prueba.php
@@ -4,8 +4,6 @@ include_once("programaRoldanReile.php");
 
 
 
-
-
 // PRINCIPAL
 
 $coleccionPalabras = cargarColeccionPalabras();
@@ -15,20 +13,37 @@ do {
     $opcion = seleccionarOpcion();
     switch ($opcion) {
         case 1: 
+            //string $nombreHugador
+            //int $cantPalabras, $numPalabras
+            //array $partida
                 $nombreJugador = solicitarJugador();
                 $cantPalabras = count($coleccionPalabras);
                 $numPalabra = solicitarNumeroEntre(0, $cantPalabras-1);
                 $partida = jugarWordix($coleccionPalabras[$numPalabra], strtolower($nombreJugador));
-                array_push($partidasCargadas[$partida]);
-                print_r($partidasCargadas);
+                $partidasCargadas[count($partidasCargadas)]=["palabraWordix"=>$partida["palabraWordix"], "jugador"=>$nombre, "intentos"=>$partida["intentos"], "puntaje"=>$partida["puntaje"]];
             break;
         case 2: 
             //completar qué secuencia de pasos ejecutar si el usuario elige la opción 2
 
             break;
         case 3: 
-            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
+            $cantPartidas = count($partidasCargadas);
 
+            echo "\nIngrese un número de partida: \n";
+            $numPartida = trim(fgets(STDIN));
+
+            if ($numPartida < 0 || $numPartida >= $cantPartidas) {
+               do {
+                echo "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓\n";
+                echo "Error, no existe esa partida. \n";
+                echo "Vuelva a ingresar un número de partida: \n";
+                echo "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓\n";
+                $numPartida = trim(fgets(STDIN));
+                } while ($numPartida < 0 || $numPartida >= $cantPartidas);
+            }
+
+            echo "Número de partida seleccionado: $numPartida\n";
+            mostrarPartida($numPartida, $partidasCargadas);
             break;
         case 4: 
             //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
